36dc08af917d00c95f88f3fd66f8960ce316cac7
matusalik <mateuszsalik@gmail.com>
2025-07-07 09:23:47 +0200

    Minor refactorization, renaming controllers

---
+++ gym_management_api/src/main/java/com/gymmanagement/gym_management_api/Repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.gymmanagement.gym_management_api.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gymmanagement.gym_management_api.Entities.User;
+
+public interface UsersRepository extends JpaRepository<User, Integer>{
+    
+}
