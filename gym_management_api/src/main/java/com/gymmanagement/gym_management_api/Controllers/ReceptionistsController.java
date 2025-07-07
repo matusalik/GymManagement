31f740ed5754f174342eddf733fad28ff4718d70
matusalik <mateuszsalik@gmail.com>
2025-07-07 08:29:33 +0200

    Added controllers

---
+++ gym_management_api/src/main/java/com/gymmanagement/gym_management_api/Controllers/ReceptionistsController.java
@@ -0,0 +1,10 @@
+package com.gymmanagement.gym_management_api.Controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/receptionists")
+public class ReceptionistsController {
+    
+}
